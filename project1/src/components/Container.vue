1e0b2a8c5e562950258ca5c28b63c12054bb8eba
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-02-12 09:00:36 -0500

    1

--- project1/src/components/Container.vue
+++ project1/src/components/Container.vue
@@ -1,14 +1,18 @@
-html, body, *{
-  margin:0;
-  padding: 0;
-  font-size: 62.5%;
-  box-sizing: border-box;
-}
+<template>
+    <div>
 
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
 .container{
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     padding: 2rem
 }
-
+</style>
